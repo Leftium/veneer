4b8e3bffd9448de938e6cc952fad9e4a7c99ef71
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-01-26 03:06:24 +0900

    Refactor gg module some more.
    - Also tweaked copy.
    - Get working in FireFox.

--- src/lib/gg.ts
+++ src/lib/gg.ts
@@ -1,116 +1,69 @@
-import type { CallSite } from 'callsite'
-
 import * as dotenv from 'dotenv'
 
 import debugFactory from 'debug'
+import StackTrace from 'stacktrace-js'
 
 import { browser } from '$app/environment'
 
-const GG_ENABLED = true
-
-function callSiteCommonPathPrefixMatches(callSite: CallSite | string) {
-	return String(callSite).match(/[@(](?<prefix>.*?\/)(?:lib|chunks)\//i)
+const ggConfig = {
+	enabled: true,
+	showPrefixTest: false,
+	showStatusAndTroubleShootingMessageOnLoad: true,
+	sourceRootMatcher: /.*?\/src|chunks\//i,
 }
-
-/** console.log('Check prefix for callSites on different platforms/environments:')
-const callSites: Record<string, Record<string, string>> = {
-	localhost: {
-		node: 'getStack (S:/p/bangtastic/src/lib/util.ts:21:17)',
-		chrome: 'getStack (http://192.168.0.5:5173/src/lib/util.ts?t=1707273649942:18:17)',
-		safari: 'getStack@http://192.168.0.5:5173/src/lib/util.ts:18:26',
-	},
-	vercel: {
-		node: 'getStack (file:///var/task/.svelte-kit/output/server/chunks/util.js:18:17)',
-		chrome: 'y (https://bangtastic.vercel.app/_app/immutable/chunks/util.pMXJCmiz.js:1:6712)',
-		safari: 'y@https://bangtastic.vercel.app/_app/immutable/chunks/util.pMXJCmiz.js:1:6721',
-	},
-}
-
-console.table(
-	Object.entries(callSites)
-		.map(([platform, environments]) =>
-			Object.entries(environments).map(([environment, callSite]) => ({
-				platform,
-				environment,
-				prefix: callSiteCommonPathPrefixMatches(callSite)?.groups?.prefix,
-				callSite,
-			})),
-		)
-		.flat(),
-)
-
-/**/
-
-// Show some info about gg
-let ggMessage = `Loaded gg module. To enable logger/debugger output:
-${GG_ENABLED ? '✅' : '❌'} In code:    GG_ENABLED = true`
-
-const ggInfo: Record<string, unknown> = {
-	GG_ENABLED,
-}
-
-if (browser) {
-	const debug = (ggInfo['localStorage.debug'] = localStorage?.getItem('debug'))
-	ggMessage += `\n${debug ? '✅' : '❌'} In browser: localStorage.debug = '*'`
-} else {
-	dotenv.config() // Load the environment variables
-	const debug = (ggInfo['process.env.DEBUG'] = process?.env?.DEBUG)
-	ggMessage += `\n${debug ? '✅' : '❌'} On server:  DEBUG=*'`
-}
-console.log(ggMessage)
-
-function getStack() {
-	// Get stack array
-	const savedPrepareStackTrace = Error.prepareStackTrace
-	Error.prepareStackTrace = (error, stack) => stack
-	const error = new Error()
-	let stack = error.stack as unknown as string | CallSite[] | string[]
-
-	if (typeof stack === 'string') {
-		stack = stack.split('\n')
-	}
-
-	Error.prepareStackTrace = savedPrepareStackTrace
-	return stack || []
-}
-
-const timestampColumnNumberRegex = /(\?t=\d+)?(:\d+):\d+\)?$/
-const swapPathFunctionNameRegex = /([ \][_.\S]+) \((.*)/
-const swapPathFunctionNameRegexSafari = /([^@]+)@(.*)/
-const lineNumberRegex = /:\d+ ?/
-
-const callSiteCommonPathPrefix =
-	callSiteCommonPathPrefixMatches(getStack()[0])?.groups?.prefix || ''
 
 const ggLog = debugFactory('gg')
 
+if (ggConfig.showStatusAndTroubleShootingMessageOnLoad) {
+	const ggLogTest = ggLog.extend('TEST')
+
+	let ggMessage =
+		`Loaded gg module. To enable logger/debugger output:\n` +
+		`${ggConfig.enabled ? '✅' : '❌'} ggConfig.enabled: ${ggConfig.enabled}`
+
+	if (!ggConfig.enabled) {
+		ggMessage += ' (Update value in gg.ts file.)'
+	}
+
+	const suggestedValue = ggLogTest.enabled ? '' : ` (Try setting to 'gg:*')`
+
+	if (browser) {
+		ggMessage += `\n${ggLogTest.enabled ? '✅' : '❌'} localStorage.debug: ${localStorage?.debug}${suggestedValue}`
+		ggMessage += `\nℹ️ "Verbose" log level must be enabled (in the devtools JS console).`
+	} else {
+		dotenv.config() // Load the environment variables
+		ggMessage += `\n${ggLogTest.enabled ? '✅' : '❌'} DEBUG env variable: ${process?.env?.DEBUG}${suggestedValue}`
+	}
+	console.log(ggMessage)
+}
+
 const callerToLogger = new Map()
 
+const timestampRegex = /(\?t=\d+)?$/
+
 export function gg(...args: [...unknown[]]) {
-	if (!GG_ENABLED) {
+	if (!ggConfig.enabled) {
 		return args[0]
 	}
 
-	// Ignore first two stack frames, which are always calls to getstack() and gg().
-	const stack = getStack().splice(2)
+	// Ignore first stack frame, which is always call to gg().
+	const stack = StackTrace.getSync().splice(1)
 
-	const callSite = stack[0].toString() || ''
+	const stackframe = stack[0]
+	const fileName = stackframe.fileName
+		?.replace(ggConfig.sourceRootMatcher, '')
+		.replace(timestampRegex, '')
 
-	const callerCleaned = callSite.replace(timestampColumnNumberRegex, '$2') // Strip timestamp and/or column number.
-	const callerSwapped = callerCleaned.includes('@')
-		? callerCleaned.replace(swapPathFunctionNameRegexSafari, '$2 $1') // Put path in front of function name.
-		: callerCleaned.replace(swapPathFunctionNameRegex, '$2 $1')
-	const caller = callerSwapped.replace(callSiteCommonPathPrefix, '').replace(lineNumberRegex, '| ') // Remove base path and line number.
-
-	// console.table({ caller, callerClean, callerSwapped, caller })
+	const caller = `${fileName}@${stackframe.functionName}`
 
 	const ggLogCaller =
 		callerToLogger.get(caller) || callerToLogger.set(caller, ggLog.extend(caller)).get(caller)
 	if (args.length === 0) {
-		ggLogCaller(callSite)
-		return { caller: callSite, stack }
+		ggLogCaller(stackframe.fileName)
+		return stack
 	}
 
 	ggLogCaller(...(args as [formatter: unknown, ...args: unknown[]]))
+
 	return args[0]
 }
