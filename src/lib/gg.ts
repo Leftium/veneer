2ecf5c4097755283698313456ec75e29a77b0f96
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-01-25 17:30:39 +0900

    Format newly copied gg code.

--- src/lib/gg.ts
+++ src/lib/gg.ts
@@ -1,24 +1,24 @@
-import type { CallSite } from 'callsite';
+import type { CallSite } from 'callsite'
 
-import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv'
 
-import debugFactory from 'debug';
+import debugFactory from 'debug'
 
-import { browser } from '$app/environment';
+import { browser } from '$app/environment'
 
-const GG_ENABLED = true;
+const GG_ENABLED = true
 
-const timestampColumnNumberRegex = /(\?t=\d+)?(:\d+):\d+\)?$/;
-const swapPathFunctionNameRegex = /([ \][_.\S]+) \((.*)/;
-const swapPathFunctionNameRegexSafari = /([^@]+)@(.*)/;
-const lineNumberRegex = /:\d+ ?/;
+const timestampColumnNumberRegex = /(\?t=\d+)?(:\d+):\d+\)?$/
+const swapPathFunctionNameRegex = /([ \][_.\S]+) \((.*)/
+const swapPathFunctionNameRegexSafari = /([^@]+)@(.*)/
+const lineNumberRegex = /:\d+ ?/
 
 function callSiteCommonPathPrefixMatches(callSite: CallSite | string) {
-	return String(callSite).match(/[@(](?<prefix>.*?\/)(?:lib|chunks)\//i);
+	return String(callSite).match(/[@(](?<prefix>.*?\/)(?:lib|chunks)\//i)
 }
 
 /*
-import _ from 'lodash';
+import _ from 'lodash'
 
 // prettier-ignore
 // callSite formats on different environments:
@@ -37,78 +37,78 @@ const callSites = [
 console.table(
 	_.zipObject(
 		callSites,
-		callSites.map((callSite: string) => callSiteCommonPathPrefixMatches(callSite))
-	)
-);
+		callSites.map((callSite: string) => callSiteCommonPathPrefixMatches(callSite)),
+	),
+)
 */
 
 const callSiteCommonPathPrefix =
-	callSiteCommonPathPrefixMatches(getStack()[0])?.groups?.prefix || '';
+	callSiteCommonPathPrefixMatches(getStack()[0])?.groups?.prefix || ''
 
 // Show some info about gg
 let ggMessage = `To enable gg logger/debugger output:
-${GG_ENABLED ? '✅' : '❌'} In code:    GG_ENABLED = true`;
+${GG_ENABLED ? '✅' : '❌'} In code:    GG_ENABLED = true`
 
 const gg_info: Record<string, unknown> = {
-	GG_ENABLED
-};
+	GG_ENABLED,
+}
 
 if (browser) {
-	const debug = (gg_info['localStorage.debug'] = localStorage?.getItem('debug'));
-	ggMessage += `\n${debug ? '✅' : '❌'} In browser: localStorage.debug = '*'`;
+	const debug = (gg_info['localStorage.debug'] = localStorage?.getItem('debug'))
+	ggMessage += `\n${debug ? '✅' : '❌'} In browser: localStorage.debug = '*'`
 } else {
-	dotenv.config(); // Load the environment variables
-	const debug = (gg_info['process.env.DEBUG'] = process?.env?.DEBUG);
-	ggMessage += `\n${debug ? '✅' : '❌'} On server:  DEBUG=*'`;
+	dotenv.config() // Load the environment variables
+	const debug = (gg_info['process.env.DEBUG'] = process?.env?.DEBUG)
+	ggMessage += `\n${debug ? '✅' : '❌'} On server:  DEBUG=*'`
 }
 
 console.log({
 	callSite: getStack()[0].toString(),
-	callSiteCommonPathPrefix
-});
-console.table(gg_info);
-console.log(ggMessage);
+	callSiteCommonPathPrefix,
+})
+console.table(gg_info)
+console.log(ggMessage)
 
 function getStack() {
 	// Get stack array
-	const savedPrepareStackTrace = Error.prepareStackTrace;
-	Error.prepareStackTrace = (error, stack) => stack;
-	const error = new Error();
-	let stack = error.stack as unknown as string | CallSite[] | string[];
+	const savedPrepareStackTrace = Error.prepareStackTrace
+	Error.prepareStackTrace = (error, stack) => stack
+	const error = new Error()
+	let stack = error.stack as unknown as string | CallSite[] | string[]
 
 	if (typeof stack === 'string') {
-		stack = stack.split('\n');
+		stack = stack.split('\n')
 	}
 
-	Error.prepareStackTrace = savedPrepareStackTrace;
-	return stack || [];
+	Error.prepareStackTrace = savedPrepareStackTrace
+	return stack || []
 }
 
 export function gg(...args: [...unknown[]]) {
 	if (!GG_ENABLED) {
-		return args[0];
+		return args[0]
 	}
 
-	const stack = getStack();
+	const stack = getStack()
 
-	const caller = getStack()[2].toString() || '';
+	const caller = getStack()[2].toString() || ''
 
-	const callerClean = caller.replace(timestampColumnNumberRegex, '$2'); // Strip timestamp and/or column number.
+	const callerClean = caller.replace(timestampColumnNumberRegex, '$2') // Strip timestamp and/or column number.
 	const callerSwapped = callerClean.includes('@')
 		? callerClean.replace(swapPathFunctionNameRegexSafari, '$2 $1') // Put path in front of function name.
-		: callerClean.replace(swapPathFunctionNameRegex, '$2 $1');
+		: callerClean.replace(swapPathFunctionNameRegex, '$2 $1')
 	const callerFinal = callerSwapped
 		.replace(callSiteCommonPathPrefix, '')
-		.replace(lineNumberRegex, '| '); // Remove base path and line number.
+		.replace(lineNumberRegex, '| ') // Remove base path and line number.
 
 	// console.table({ caller, callerClean, callerSwapped, callerFinal })
 
-	const ggLog = debugFactory(callerFinal);
+	const ggLog = debugFactory(callerFinal)
 	if (args.length === 0) {
-		ggLog(caller);
-		return { caller, stack };
+		ggLog(caller)
+		return { caller, stack }
 	}
 
-	ggLog(...(args as [formatter: unknown, ...args: unknown[]]));
-	return args[0];
+	ggLog(...(args as [formatter: unknown, ...args: unknown[]]))
+	return args[0]
 }
