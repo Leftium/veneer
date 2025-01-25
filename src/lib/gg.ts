04e3f405b9d91ae40147cbf1345592b0a899d07c
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-01-26 01:05:42 +0900

    Refactor gg module.

--- src/lib/gg.ts
+++ src/lib/gg.ts
@@ -8,65 +8,55 @@ import { browser } from '$app/environment'
 
 const GG_ENABLED = true
 
-const timestampColumnNumberRegex = /(\?t=\d+)?(:\d+):\d+\)?$/
-const swapPathFunctionNameRegex = /([ \][_.\S]+) \((.*)/
-const swapPathFunctionNameRegexSafari = /([^@]+)@(.*)/
-const lineNumberRegex = /:\d+ ?/
-
 function callSiteCommonPathPrefixMatches(callSite: CallSite | string) {
 	return String(callSite).match(/[@(](?<prefix>.*?\/)(?:lib|chunks)\//i)
 }
 
-/*
-import _ from 'lodash'
-
-// prettier-ignore
-// callSite formats on different environments:
-const callSites = [
-	// local dev
-	'getStack (S:/p/bangtastic/src/lib/util.ts:21:17)',                         // node
-	'getStack (http://192.168.0.5:5173/src/lib/util.ts?t=1707273649942:18:17)', // chrome
-	'getStack@http://192.168.0.5:5173/src/lib/util.ts:18:26',                   // safari
-
-	// vercel (production)
-	'getStack (file:///var/task/.svelte-kit/output/server/chunks/util.js:18:17)',      // node
-	'y (https://bangtastic.vercel.app/_app/immutable/chunks/util.pMXJCmiz.js:1:6712)', // chrome
-	'y@https://bangtastic.vercel.app/_app/immutable/chunks/util.pMXJCmiz.js:1:6721'    // safari
-];
+/** console.log('Check prefix for callSites on different platforms/environments:')
+const callSites: Record<string, Record<string, string>> = {
+	localhost: {
+		node: 'getStack (S:/p/bangtastic/src/lib/util.ts:21:17)',
+		chrome: 'getStack (http://192.168.0.5:5173/src/lib/util.ts?t=1707273649942:18:17)',
+		safari: 'getStack@http://192.168.0.5:5173/src/lib/util.ts:18:26',
+	},
+	vercel: {
+		node: 'getStack (file:///var/task/.svelte-kit/output/server/chunks/util.js:18:17)',
+		chrome: 'y (https://bangtastic.vercel.app/_app/immutable/chunks/util.pMXJCmiz.js:1:6712)',
+		safari: 'y@https://bangtastic.vercel.app/_app/immutable/chunks/util.pMXJCmiz.js:1:6721',
+	},
+}
 
 console.table(
-	_.zipObject(
-		callSites,
-		callSites.map((callSite: string) => callSiteCommonPathPrefixMatches(callSite)),
-	),
+	Object.entries(callSites)
+		.map(([platform, environments]) =>
+			Object.entries(environments).map(([environment, callSite]) => ({
+				platform,
+				environment,
+				prefix: callSiteCommonPathPrefixMatches(callSite)?.groups?.prefix,
+				callSite,
+			})),
+		)
+		.flat(),
 )
-*/
 
-const callSiteCommonPathPrefix =
-	callSiteCommonPathPrefixMatches(getStack()[0])?.groups?.prefix || ''
+/**/
 
 // Show some info about gg
-let ggMessage = `To enable gg logger/debugger output:
+let ggMessage = `Loaded gg module. To enable logger/debugger output:
 ${GG_ENABLED ? '✅' : '❌'} In code:    GG_ENABLED = true`
 
-const gg_info: Record<string, unknown> = {
+const ggInfo: Record<string, unknown> = {
 	GG_ENABLED,
 }
 
 if (browser) {
-	const debug = (gg_info['localStorage.debug'] = localStorage?.getItem('debug'))
+	const debug = (ggInfo['localStorage.debug'] = localStorage?.getItem('debug'))
 	ggMessage += `\n${debug ? '✅' : '❌'} In browser: localStorage.debug = '*'`
 } else {
 	dotenv.config() // Load the environment variables
-	const debug = (gg_info['process.env.DEBUG'] = process?.env?.DEBUG)
+	const debug = (ggInfo['process.env.DEBUG'] = process?.env?.DEBUG)
 	ggMessage += `\n${debug ? '✅' : '❌'} On server:  DEBUG=*'`
 }
-
-console.log({
-	callSite: getStack()[0].toString(),
-	callSiteCommonPathPrefix,
-})
-console.table(gg_info)
 console.log(ggMessage)
 
 function getStack() {
@@ -84,6 +74,16 @@ function getStack() {
 	return stack || []
 }
 
+const timestampColumnNumberRegex = /(\?t=\d+)?(:\d+):\d+\)?$/
+const swapPathFunctionNameRegex = /([ \][_.\S]+) \((.*)/
+const swapPathFunctionNameRegexSafari = /([^@]+)@(.*)/
+const lineNumberRegex = /:\d+ ?/
+
+const callSiteCommonPathPrefix =
+	callSiteCommonPathPrefixMatches(getStack()[0])?.groups?.prefix || ''
+
+const ggLog = debugFactory('gg')
+
 export function gg(...args: [...unknown[]]) {
 	if (!GG_ENABLED) {
 		return args[0]
@@ -92,24 +92,22 @@ export function gg(...args: [...unknown[]]) {
 	// Ignore first two stack frames, which are always calls to getstack() and gg().
 	const stack = getStack().splice(2)
 
-	const caller = stack[0].toString() || ''
+	const callSite = stack[0].toString() || ''
 
-	const callerClean = caller.replace(timestampColumnNumberRegex, '$2') // Strip timestamp and/or column number.
-	const callerSwapped = callerClean.includes('@')
-		? callerClean.replace(swapPathFunctionNameRegexSafari, '$2 $1') // Put path in front of function name.
-		: callerClean.replace(swapPathFunctionNameRegex, '$2 $1')
-	const callerFinal = callerSwapped
-		.replace(callSiteCommonPathPrefix, '')
-		.replace(lineNumberRegex, '| ') // Remove base path and line number.
+	const callerCleaned = callSite.replace(timestampColumnNumberRegex, '$2') // Strip timestamp and/or column number.
+	const callerSwapped = callerCleaned.includes('@')
+		? callerCleaned.replace(swapPathFunctionNameRegexSafari, '$2 $1') // Put path in front of function name.
+		: callerCleaned.replace(swapPathFunctionNameRegex, '$2 $1')
+	const caller = callerSwapped.replace(callSiteCommonPathPrefix, '').replace(lineNumberRegex, '| ') // Remove base path and line number.
 
-	// console.table({ caller, callerClean, callerSwapped, callerFinal })
+	// console.table({ caller, callerClean, callerSwapped, caller })
 
-	const ggLog = debugFactory(callerFinal)
+	const ggLogCaller = ggLog.extend(caller + ':')
 	if (args.length === 0) {
-		ggLog(caller)
-		return { caller, stack }
+		ggLogCaller(callSite)
+		return { caller: callSite, stack }
 	}
 
-	ggLog(...(args as [formatter: unknown, ...args: unknown[]]))
+	ggLogCaller(...(args as [formatter: unknown, ...args: unknown[]]))
 	return args[0]
 }
