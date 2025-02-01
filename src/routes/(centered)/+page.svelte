ba2343b29cbc01758eaf6e36ccba4a9f766b32eb
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-02-01 10:49:21 +0900

    Remove unneeded code from /+page.svelte.

--- src/routes/(centered)/+page.svelte
+++ src/routes/(centered)/+page.svelte
@@ -2,21 +2,8 @@
 	import AutogrowingTextarea from '$lib/components/AutogrowingTextarea.svelte'
 	import { gg } from '$lib/gg'
 
-	const namespaces = gg.disable()
-
-	// Force gg output:
-	gg.enable('gg:*')
-
-	gg('Hello from +page script tag. (Forced output, even if localStorage.debug/DEBUG not set.)')
+	gg('Hello from +page script tag!)')
 	gg()
-
-	// Restore namespaces
-	gg.enable(namespaces)
 </script>
 
 <AutogrowingTextarea />
-
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-</p>
