88f78b8d6f2c58e0cfb1ed7eddfb011fec6c124c
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-02-01 11:38:21 +0900

    Extract links from textarea.

--- src/routes/(centered)/+page.svelte
+++ src/routes/(centered)/+page.svelte
@@ -3,15 +3,25 @@
 	import { gg } from '$lib/gg'
 	import { undent } from '$lib/undent'
 
+	import * as linkify from 'linkifyjs'
+
 	let value = $state(undent`
         신청
         https://forms.gle/egp2YUahB5kn7ysNA
         확인
-        https://docs.google.com/spreadsheets/d/1Gqv0KjhVGWZrAr-f7TrORxDgn8ODE0N97QPNIHkERcE/edit
+        https://docs.google.com/spreadsheets/d/1Gqv0KjhVGWZrAr-f7TrORxDgn8ODE0N97QPNIHkERcE
     `)
+
+	const links = $derived(linkify.find(value))
 
 	gg('Hello from +page script tag!)')
 	gg()
 </script>
 
 <AutogrowingTextarea bind:value />
+
+{#each links as link}
+	<div><a href={link.href}>{link.href}</a></div>
+{/each}
+
+<pre>{JSON.stringify(links, null, 4)}</pre>
