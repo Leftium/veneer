ddffb5edadb4eee41cbc8994de9d0dbaecd47de5
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-02-02 00:37:14 +0900

    Fetch text and json sheet.

--- src/routes/api/fetch-text/+server.ts
+++ src/routes/api/fetch-text/+server.ts
@@ -1,4 +1,3 @@
-import { gg } from '$lib/gg.js'
 import { json } from '@sveltejs/kit'
 
 import { GCP_API_KEY } from '$env/static/private'
@@ -57,6 +56,76 @@ function infoFromGoogleUrl(url: string) {
 	return { type, id, urlCanonical, urlFetch }
 }
 
+// https://stackoverflow.com/a/16233621/117030
+function excelDateToJsDate(serial: number) {
+	const utc_days = Math.floor(serial - 25569)
+	const utc_value = utc_days * 86400
+	const date_info = new Date(utc_value * 1000)
+
+	const fractional_day = serial - Math.floor(serial) + 0.0000001
+
+	let total_seconds = Math.floor(86400 * fractional_day)
+
+	const seconds = total_seconds % 60
+
+	total_seconds -= seconds
+
+	const hours = Math.floor(total_seconds / (60 * 60))
+	const minutes = Math.floor(total_seconds / 60) % 60
+
+	return new Date(
+		date_info.getFullYear(),
+		date_info.getMonth(),
+		date_info.getDate(),
+		hours,
+		minutes,
+		seconds,
+	)
+}
+
+type GoogleSheetsApiResult = {
+	properties: { title: string }
+	sheets: {
+		properties: { title: string }
+		data: {
+			rowData: {
+				values: {
+					formattedValue: string
+					effectiveValue: {
+						numberValue?: number
+					}
+				}[]
+			}[]
+			rowMetadata: { hiddenByUser?: boolean }[]
+			columnMetadata: { hiddenByUser?: boolean }[]
+		}[]
+	}[]
+}
+
+function adjustGoogleSheetData(json: GoogleSheetsApiResult) {
+	const data = json?.sheets?.[0]?.data[0]
+	if (!data) {
+		return json
+	}
+
+	const title = json.properties.title
+	const sheetTitle = json.sheets[0].properties.title
+
+	const hiddenColumns = data.columnMetadata.flatMap((cm, i) => (cm.hiddenByUser ? i : []))
+	const hiddenRows = data.rowMetadata.flatMap((rm, i) => (rm.hiddenByUser ? i : []))
+
+	const values = data.rowData.map((rowDatum) => {
+		return rowDatum.values.map((value) => {
+			const excelSerialDate = value?.effectiveValue?.numberValue
+			return excelSerialDate
+				? [value.formattedValue, excelDateToJsDate(excelSerialDate)]
+				: value.formattedValue
+		})
+	})
+
+	return { title, sheetTitle, values, hiddenColumns, hiddenRows }
+}
+
 export const GET = async ({ url, fetch }) => {
 	const urls = url.searchParams.getAll('u')
 
@@ -73,6 +142,16 @@ export const GET = async ({ url, fetch }) => {
 				;({ type, id, urlCanonical } = infoFromGoogleUrl(urlFetched))
 			}
 
+			const fetched = await fetch(urlFetch)
+			const text = await fetched.text()
+
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			let json: any = {}
+			if (type === 'sheet') {
+				json = JSON.parse(text)
+				json = adjustGoogleSheetData(json)
+			}
+
 			return {
 				type,
 				id,
@@ -81,6 +160,8 @@ export const GET = async ({ url, fetch }) => {
 				urlFetch,
 				urlFetched,
 				urlCanonical,
+				text,
+				json,
 			}
 		}),
 	)
