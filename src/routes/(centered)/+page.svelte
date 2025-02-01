17bbf1b25d9634e0c5c84a403e92187c487067e0
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-02-01 11:19:37 +0900

    Give textarea an intial value.

--- src/routes/(centered)/+page.svelte
+++ src/routes/(centered)/+page.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import AutogrowingTextarea from '$lib/components/AutogrowingTextarea.svelte'
 	import { gg } from '$lib/gg'
+	import { undent } from '$lib/undent'
+
+	let value = $state(undent`
+        신청
+        https://forms.gle/egp2YUahB5kn7ysNA
+        확인
+        https://docs.google.com/spreadsheets/d/1Gqv0KjhVGWZrAr-f7TrORxDgn8ODE0N97QPNIHkERcE/edit
+    `)
 
 	gg('Hello from +page script tag!)')
 	gg()
 </script>
 
-<AutogrowingTextarea />
+<AutogrowingTextarea bind:value />
