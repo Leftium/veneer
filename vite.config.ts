cda1b394cb6c5e4f1a032a504c0c7cec080c0678
Leftium <381217+Leftium@users.noreply.github.com>
2025-07-17 18:54:23 -0500

    Add devtools-json.
    - npx sv add devtools-json
    - https://svelte.dev/docs/cli/devtools-json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+import devtoolsJson from 'vite-plugin-devtools-json'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
-
 import openInEditorPlugin from './src/lib/open-in-editor'
 
 export default defineConfig({
-	plugins: [sveltekit(), openInEditorPlugin()],
+	plugins: [sveltekit(), openInEditorPlugin(), devtoolsJson()],
 	esbuild: {
 		supported: {
 			'top-level-await': true, //browsers can handle top-level-await features
