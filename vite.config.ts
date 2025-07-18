2a98ef2b4daa3749fc128f1c2c32f4ac5f42d038
Leftium <381217+Leftium@users.noreply.github.com>
2025-07-18 18:25:55 -0500

    Use openInEditorPlugin from '@leftium/gg'.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import devtoolsJson from 'vite-plugin-devtools-json'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
-import openInEditorPlugin from './src/lib/open-in-editor'
+import { openInEditorPlugin } from '@leftium/gg'
+
 
 export default defineConfig({
 	plugins: [sveltekit(), openInEditorPlugin(), devtoolsJson()],
