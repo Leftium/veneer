0ab0004e18cb3c1dc528f46d0f72379f96abd7b8
Leftium <381217+Leftium@users.noreply.github.com>
2025-08-01 15:54:58 -0500

    Add more links and add labels.

--- src/routes/(centered)/+page.svelte
+++ src/routes/(centered)/+page.svelte
@@ -4,6 +4,8 @@
 </script>
 
 {@html md`
-	- /15/g.zUuXZZuvobvnU1ja7
-	- /15/g.r6eRUz2U9uf5oVFn6/s.13E_wsbrKLEsuV-eDaTKl0a967EdpYgcZrXH0Gq_KK3g
+	- 9월 14일 소셜 [DEV] /15/g.zUuXZZuvobvnU1ja7
+	- 8월 10일 일요일 /15/g.r6eRUz2U9uf5oVFn6/s.13E_wsbrKLEsuV-eDaTKl0a967EdpYgcZrXH0Gq_KK3g
+	- Midnight Bubbles(응답) /12/s.1_bZqtj2UoVjNLm1GRqbAI-QjNSoyOa2veWAfvyhFQQo
+	- open sheet test /12/s.1o5t26He2DzTweYeleXOGiDjlU4Jkx896f95VUHVgS8U
 `}
