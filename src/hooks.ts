304d029a4e1b235c1e43b9361a1a32df74369931
Leftium <381217+Leftium@users.noreply.github.com>
2025-08-05 00:59:32 -0500

    Add paraglide.
    - sv add paraglide=demo:yes

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,3 +1,4 @@
+import { deLocalizeUrl } from '$lib/paraglide/runtime'
 // src/hooks.ts
 import { ok, err } from 'neverthrow'
 import type { Transport } from '@sveltejs/kit'
@@ -9,17 +10,22 @@ export const transport: Transport = {
 				if (r.isOk && r.isOk()) {
 					return { value: r.value }
 				}
+
 				if (r.isErr && r.isErr()) {
 					return { error: r.error }
 				}
 			}
+
 			return false
 		},
 		decode: (data) => {
 			if ('value' in data) {
 				return ok(data.value)
 			}
+
 			return err(data.error)
 		},
 	},
 }
+
+export const reroute = (request) => deLocalizeUrl(request.url).pathname
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js'
 import devtoolsJson from 'vite-plugin-devtools-json'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 import { openInEditorPlugin } from '@leftium/gg'
 
 export default defineConfig({
-	plugins: [sveltekit(), openInEditorPlugin(), devtoolsJson()],
+	plugins: [
+		sveltekit(),
+		openInEditorPlugin(),
+		devtoolsJson(),
+		paraglideVitePlugin({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide',
+		}),
+	],
 	esbuild: {
 		supported: {
 			'top-level-await': true, //browsers can handle top-level-await features
