3fad41ebd90542ab3ce27855299d9044998d4ea4
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-03-31 06:49:14 +0900

    refactor: remove unused ALL_BOTH_POOL export from dance-party

--- src/lib/dance-party.ts
+++ src/lib/dance-party.ts
@@ -227,12 +227,6 @@ export const PAIRED_POOL = [
 	32,
 ] as const
 
-/** All 32 both-images (breakaway + connected). */
-export const ALL_BOTH_POOL = [
-	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
-	28, 29, 30, 31, 32,
-] as const
-
 /**
  * true = leader (pants figure) is on the LEFT side of the unflipped image.
  */
